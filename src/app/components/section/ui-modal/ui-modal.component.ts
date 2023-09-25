53d48c1729f879477fc8486df1aaabf40acc8834
solo-leveler <shinichiizumi@protonmail.ch>
2023-09-26 00:38:59 +0530

    Revert "added ui-modal ts file"
    This reverts commit 538e84a48cf1676ccd75d4193f0169ae4d902337.

--- src/app/components/section/ui-modal/ui-modal.component.ts
+++ src/app/components/section/ui-modal/ui-modal.component.ts
@@ -1,38 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ui-modal',
   templateUrl: './ui-modal.component.html',
   styleUrls: ['./ui-modal.component.scss']
 })
-export class UiModalComponent implements OnInit {
-  @Input() dialogClass !: string;
-  @Input() hideHeader = false;
-  @Input() hideFooter = false;
-  @Input() containerClick = true;
-  public visible = false;
-  public visibleAnimate = false;
-
-  constructor() { }
-
-  ngOnInit(): void {}
-
-  public show(): void {
-    this.visible = true;
-    setTimeout(() => this.visibleAnimate = true, 100);
-    document?.querySelector('body')?.classList.add('modal-open');
-  }
-
-  public hide(): void {
-    this.visibleAnimate = false;
-    setTimeout(() => this.visible = false, 300);
-    document?.querySelector('body')?.classList.remove('modal-open');
-  }
-
-  public onContainerClicked(event: MouseEvent): void {
-    if ((event.target as HTMLElement).classList.contains('modal') && this.containerClick === true) {
-      this.hide();
-    }
-  }
+export class UiModalComponent {
 
 }
